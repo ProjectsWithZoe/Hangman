2d33d507cfbef7c2739a59007a635fefa1a0ff50
ProjectsWithZoe <118231395+ProjectsWithZoe@users.noreply.github.com>
2023-01-04 19:55:12 +0000

    random word printed

--- milestone_2.py
+++ milestone_2.py
@@ -3,5 +3,5 @@ import random
 word_list = ['banana', 'grape', 'mango', 'mandarin', 'strawberry']
 print(word_list)
 
-random_word = random.choice(word_list)
-print(random_word)
+word = random.choice(word_list)
+print(word)
