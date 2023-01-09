5b25244ccecd92628b20600f89d7a6762a7c91f0
ProjectsWithZoe <118231395+ProjectsWithZoe@users.noreply.github.com>
2023-01-09 11:23:29 +0000

    Task 4 - functions created

--- milestone_3.py
+++ milestone_3.py
@@ -3,14 +3,21 @@ import random
 word_list = ['banana', 'grape', 'mango', 'mandarin', 'strawberry']
 word = random.choice(word_list)
 
-while True:
-    guess = input('Guess a letter')
-    if len(guess) == 1 and guess.isalpha():
-        break
+guess = input('Guess a letter')  
+
+def check_guess (guess):
+    guess.lower()
+    if guess in word:
+        print(f'Good guess!{guess} is in the word.')
     else:
-        print("Invalid letter. Please, enter a single alphabetical character.")
-    
-if guess in word:
-    print(f'Good guess!{guess} is in the word.')
-else:
-    print(f'Sorry, {guess} is not in the word. Try again.')
+        print(f'Sorry, {guess} is not in the word. Try again.')
+
+def ask_for_iput():
+    while True:
+        if len(guess) == 1 and guess.isalpha():
+            break
+        else:
+            print("Invalid letter. Please, enter a single alphabetical character.")
+    check_guess(guess)
+
+ask_for_iput()
