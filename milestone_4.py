d95c2c8d989f8444fb8695f2424f0ebf161a9b15
ProjectsWithZoe <118231395+ProjectsWithZoe@users.noreply.github.com>
2023-01-09 20:27:41 +0000

    else block added

--- milestone_4.py
+++ milestone_4.py
@@ -15,10 +15,16 @@ class Hangman:
         guess.lower()
         if guess in self.word:
             print(f'Good guess! {guess} is in the word.')
+        else:
+            self.num_lives-=1
+            print(f'Sorry, {guess} is not in the word.')
+            print (f'You have {self.num_lives} lives left.')
+        self.list_of_guesses.append(guess)
         for i in range(len(self.word)):
             if self.word[i] == guess:
                 self.word_guessed[i] = guess
         self.num_letters-=1
+
 
 
     def ask_for_input(self):
