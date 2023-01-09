96a9806d0f1751d482d337c99d13b50761631d19
ProjectsWithZoe <118231395+ProjectsWithZoe@users.noreply.github.com>
2023-01-09 19:03:58 +0000

    Hangman class created

--- milestone_4.py
+++ milestone_4.py
@@ -1,4 +1,5 @@
 import random
+from milestone_3 import word_list
 
 class Hangman:
     def __init__(self,word_list, num_lives=5):
@@ -8,5 +9,22 @@ class Hangman:
         self.num_lives = num_lives
         self.list_of_guesses = []
         self.word_list = word_list
-        
 
+    def check_guess(self,guess):
+        guess.lower()
+        if guess in self.word:
+            print(f'Good guess!{guess} is in the word.')
+
+    def ask_for_input(self):
+        while True:
+            guess = input('Guess a letter')
+            if guess.isalpha() == False and len(guess)!=1:
+                print("Invalid letter. Please, enter a single alphabetical character.")
+            elif guess in self.list_of_guesses:
+                print('You already tried that letter!')
+            else:
+                hangman.check_guess(guess)
+                self.list_of_guesses.append(guess)
+
+hangman = Hangman(word_list)
+hangman.ask_for_input()
