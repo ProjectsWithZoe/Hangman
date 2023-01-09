d82885fd87a1c7d989f6536eb4798d85b4c6418d
ProjectsWithZoe <118231395+ProjectsWithZoe@users.noreply.github.com>
2023-01-09 20:21:39 +0000

    letter replaces underscore

--- milestone_4.py
+++ milestone_4.py
@@ -15,6 +15,11 @@ class Hangman:
         guess.lower()
         if guess in self.word:
             print(f'Good guess! {guess} is in the word.')
+        for i in range(len(self.word)):
+            if self.word[i] == guess:
+                self.word_guessed[i] = guess
+        self.num_letters-=1
+
 
     def ask_for_input(self):
         while True:
